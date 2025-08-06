3e2e4120a1d7a16aba862ef11b0164defd0ab33e
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-08-07 00:59:03 +0530

    git commit -m "Add Number Guessing Game mini project"#

---
+++ vasco_de_gama/Python Mini Projects/Number-Guessing-Game.py
@@ -0,0 +1,17 @@
+import random
+secret_number = random.randint(1,100)
+count = 0
+while True:
+    try:
+        guess = int(input("Guess Number between 1 to 100:"))
+        count += 1
+        if guess>secret_number:
+            print("Number is too HIGH!")
+        elif guess<secret_number:
+            print("Number is too LOW!")
+        else :
+            print(f"Successfully Guessed! Number is {secret_number}")
+            print(f"It took {count} Guesses to Win!")
+            break
+    except ValueError:
+        print("Invalid Input:Give Numbers only")
